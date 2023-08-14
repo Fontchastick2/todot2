fb20e57df3743810d3485f314168c40552450321
fontchastick <toubibharmony@gmail.com>
2023-08-14 17:27:00 +0200

    DEV-1 made sql work

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, ScrollView, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as SQLite from 'expo-sqlite'
+const db = SQLite.openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024) // returns Database object
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      data: null
+    }
+
+    db.transaction(function (tx) { 
+      tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)'); 
+      tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "foobar")'); 
+      tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")'); 
+   });
+  }
+
+  transaction() {
+    db.transaction(function (tx) { 
+      tx.executeSql('SELECT * FROM LOGS',[], (_, { rows }) =>
+      console.log(JSON.stringify(rows))
+    ); 
+    });
+  }
+
+  render() {
+    return (
+        <View>
+        <Text>Add Random Name with Counts</Text>
+        <TouchableOpacity>
+        <Button onPress={this.transaction} title="Press Me" />        
+        </TouchableOpacity>
+      </View >
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+// Styles are removed purpose-fully 
